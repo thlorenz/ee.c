c81e8da4f9d118074175739c3c9e4d265e825ea8
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-17 15:00:17 -0500

    copying deps src for rgrind

--- valgrind.mk
+++ valgrind.mk
@@ -4,7 +4,7 @@ VFLAGS = --track-origins=yes --tool=memcheck --leak-check=yes --error-exitcode=1
 
 rsync:
 	ssh udesktop 'rm -rf tmp/ee'
-	rsync -ra -e ssh --exclude '/.git' --exclude '/bin' --exclude '/deps' --exclude '/build' . udesktop:tmp/ee
+	rsync -ra -e ssh --exclude '/.git' --exclude '/bin' --exclude '/deps/**/*.o' --exclude '/build' . udesktop:tmp/ee
 
 grind: $(LIST) $(OBJS) $(TESTS)
 	set -e; for file in bin/test/*; do echo "\n\033[00;32m+++ $$file +++\033[00m\n" && valgrind $(VFLAGS) ./$$file; done
